abf920b9d9b9a4d5fc54c3180c81987ae9f80807
Mariusz Woloszyn <emsi@users.noreply.github.com>
2024-06-28 17:04:08 +0000

    Updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  target: 'electron-renderer',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  entry: './src/App.tsx',
-  target: 'electron-renderer',
   module: {
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'ts-loader'
+          loader: 'ts-loader',
         },
       },
     ],
   },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
     }),
   ],
 };
